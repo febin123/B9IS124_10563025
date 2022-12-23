31ff53814cd69cfba820c7df2017e6a1657a8d73
febin123 <febsep96@gmail.com>
2022-12-23 15:01:26 +0530

    added api key in getdataservice

--- src/app/getdata.service.ts
+++ src/app/getdata.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Http } from '@capacitor-community/http';
+import { from } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class GetdataService {
 
-  constructor() { }
+  constructor() {}
+
+  doGet=() => {
+    const options={ 
+    url: 'https://newsapi.org/v2/everything?q=ireland&from=2022-12-01&sortBy=publishedAt&apiKey=9f3557dcca5a428da3f905f6f373f594',
+    // headers: { 'X-Fake-Header': 'Max was here' },
+    // params: { size: 'XL' },
+  };
+
+return from(Http.get(options))
+};
 }
